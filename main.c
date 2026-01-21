5ae1b447bd90f975b6af013a68c380fb149f4bed
mrsaiba <andreasqp@gmail.com>
2026-01-21 21:31:00 +0200

    Addded real time cursor position

--- main.c
+++ main.c
@@ -15,8 +15,8 @@ editor_state state = RAW;
 void display_windows_size() {
     attron(A_BOLD);
     char windows_size[32];
-    snprintf(windows_size, 32, "C:%i,R:%i", col, row); 
-    mvprintw(row - 1, col - strlen(windows_size), "%s", windows_size);
+    snprintf(windows_size, 32, "R:%i C:%i, %i:%i", max_row, max_col, row, col); 
+    mvprintw(max_row - 1, max_col - strlen(windows_size), "%s", windows_size);
     attroff(A_BOLD);
 }
 
@@ -39,21 +39,43 @@ void display_data() {
     display_windows_size();
     display_row_number();
     display_state();
-    move(0,1); // TO-DO: wrapper when calling move, to auto update row and col
-    //row = 0; col = 1;
+    move(row, col); // TO-DO: wrapper when calling move, to auto update row and col
 }
 
 int main() {
     initscr();
     getmaxyx(stdscr,max_row,max_col);		/* get the number of rows and columns */
     noecho(); // user input not displayed
+    cbreak();
+    keypad(stdscr, TRUE);
     mvprintw(max_row/2, (max_col- 1 -strlen(EDITOR_NAME))/2, "%s", EDITOR_NAME);
     display_data();
     refresh();
+    int input = getch();
+    clear();
     while (1) {
-        int input = getch();
         int loop_state = 1;
         switch(input) {
+            case KEY_UP:
+                if (row == 0)
+                    break;
+                row--;
+                break;
+            case KEY_DOWN:
+                if (row == max_row)
+                    break;
+                row++;
+                break;
+            case KEY_RIGHT:
+                if (col == max_col)
+                    break;
+                col++;
+                break;
+            case KEY_LEFT:
+                if (col == 0)
+                    break;
+                col--;
+                break;
             case 'q':
                 loop_state = 0;
                 break;
@@ -62,9 +84,9 @@ int main() {
         }
         if (!loop_state)
             break;
-        clear();
         display_data();
         refresh();
+        input = getch();
     }
     endwin(); // close the screen
     return 0;
